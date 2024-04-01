0c69a480817339071bfae52eec312486e0e8683c
Alex O Bunnyshell <eaasmigrations@bunnyshell.com>
2024-04-02 00:16:01 +0300

    update script to support KEY, EMAIL or TOKEN auth

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# cloudflare-tunnel-dns
-Scripts related to managing DNS records for CF tunnels
+# Cloudflare Tunnel DNS Record Creation Script
+
+This project contains a bash script for creating DNS records in Cloudflare and pointing to a specified Cloudflare Tunnel.
+The script parses the Bunnyshell env definition file (passed in as $COMPONENTS) to extract the list or DNS records to be created.
+Next, the script interacts with the Cloudflare API to create DNS records.
+
+## Prerequisites
+
+- Bash
+- jq
+- curl
+
+## Environment Variables
+
+The script requires the following environment variables:
+
+- `COMPONENTS`: The Bunnyshell yaml env configuration, generated like this:  `COMPONENTS: '{{ component.name}}|{{ components|json_encode }}'` .
+- `CF_ACCOUNT_ID`: Your Cloudflare account ID.
+- `CF_TUNNEL_ID`: The ID of the Cloudflare tunnel.
+- `CF_ZONE_ID`: The ID of the Cloudflare zone.
+- one of two ways of authentication:
+  - `CF_API_KEY` and `CF_EMAIL`: Your (Global) Cloudflare API key.
+  - of `CF_API_TOKEN`: a CF API token with necessary permissions: read account settings, read Tunnel settings and edit zone.
+  
+## Usage
+
+To run the script, simply execute the `create_records.sh` file in your terminal/pipeline:
+
+```bash
+./create_records.sh
+```
+
+The script will print out the DNS records that need to be created, retrieve the initial settings from Cloudflare, and then create a record for each URL in the list. If a URL matches the pattern `bunnyenv.com`, it will be skipped.
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details.
--- create_records.sh
+++ create_records.sh
@@ -1,13 +1,40 @@
 #!/bin/bash
-set -euo pipefail
+set -o pipefail
 
-#"${COMPONENTS:?Variable not set}"
+# if CF_EMAIL and CF_API_KEY are not set, then we will use the API token
 
-#"${CF_API_KEY:?Variable not set}"
-#"${CF_ACCOUNT_ID:?Variable not set}"
-#"${CF_TUNNEL_ID:?Variable not set}"
-#"${CF_ZONE_ID:?Variable not set}"
+if [[ -n "${CF_API_KEY}" && -n "${CF_EMAIL}" ]]; then
+  echo "using CF_API_KEY and CD_EMAIL for authentication"
+  #AUTHORIZATION_HEADER="--header 'X-Auth-Email: ${CF_EMAIL}' --header 'X-Auth-Key: ${CF_API_KEY}'"
+  AUTHORIZATION_HEADER=(--header "X-Auth-Email: ${CF_EMAIL}" --header "X-Auth-Key: ${CF_API_KEY}")
+elif [[ -n "${CF_API_TOKEN}" ]]; then
+  echo "using CF_API_TOKEN for authentication"
+  #AUTHORIZATION_HEADER="--header 'Authorization: Bearer $CF_API_KEY'"
+  AUTHORIZATION_HEADER=(--header "Authorization: Bearer $CF_API_KEY")
+else
+  echo "(CF_API_KEY, CF_EMAIL) or CF_API_TOKEN must be set" >&2
+  exit 1
+fi
 
+if [[ -z "${CF_ZONE_ID}" ]]; then
+  echo "CF_ZONE_ID must be set" >&2
+  exit 1
+fi
+
+if [[ -z "${CF_ACCOUNT_ID}" ]]; then
+  echo "CF_ACCOUNT_ID must be set" >&2
+  exit 1
+fi
+
+if [[ -z "${CF_TUNNEL_ID}" ]]; then
+  echo "CF_TUNNEL_ID must be set" >&2
+  exit 1
+fi
+
+if [[ -z "${COMPONENTS}" ]]; then
+  echo "COMPONENTS must be set" >&2
+  exit 1
+fi
 
 printenv | grep -v COMPONENTS
 DNS_RECORDS=$(echo $COMPONENTS | cut -d'|' -f2- | jq --raw-output 'to_entries | map({key, value: .value.ingress.hosts[].hostname}) | .[].value')
@@ -24,7 +51,7 @@ echo "Retrieving initial settings"
 INITIAL_SETTINGS=$(curl -s -S --fail --request GET \
     --url https://api.cloudflare.com/client/v4/accounts/$CF_ACCOUNT_ID/cfd_tunnel/$CF_TUNNEL_ID/configurations \
     --header "Content-Type: application/json" \
-    --header "Authorization: Bearer $CF_API_KEY")
+    "${AUTHORIZATION_HEADER[@]}")
 
 if [ $? -ne 0 ]; then
   echo "Failed to retrieve initial settings" >&2
@@ -50,18 +77,19 @@ for FQDN in $DNS_RECORDS; do
     echo "creating record: $FQDN"
 
     # remove the domain name from the FQDN
-    RECORD_NAME=$(echo $FQDN | sed 's/[.]/\n/g' | head -n -2 | paste -sd '.' -)
+    #RECORD_NAME=$(echo $FQDN | sed 's/[.]/\n/g' | head -n -2 | paste -sd '.' -)
+    RECORD_NAME=$(echo $FQDN | awk -F. 'BEGIN{OFS="."} NF{NF-=2}1')
     echo "RECORD_NAME: $RECORD_NAME"
 
 
     CREATE_RESULT=$(curl -s -S --fail --request POST \
         --url https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/dns_records \
         --header 'Content-Type: application/json' \
-        --header "Authorization: Bearer $CF_API_KEY" \
+        "${AUTHORIZATION_HEADER[@]}" \
         --data "{ \"type\": \"CNAME\", \"name\": \"${RECORD_NAME}\", \"content\": \"${CF_TUNNEL_ID}.cfargotunnel.com\", \"proxied\": true }")
 
     if [ $? -ne 0 ]; then
-        echo "Failed to create record for $FQDN" >&2
+        echo "Failed to create record for $FQDN (record may already exist)", >&2
         echo "CREATE_RESULT: $CREATE_RESULT" >&2
     else
         echo "CREATED $FQDN"
@@ -75,7 +103,7 @@ echo "preserving initial settings"
 FINAL_RESULT=$(curl -s -S --fail --request PUT \
     --url https://api.cloudflare.com/client/v4/accounts/$CF_ACCOUNT_ID/cfd_tunnel/$CF_TUNNEL_ID/configurations \
     --header 'Content-Type: application/json' \
-    --header "Authorization: Bearer $CF_API_KEY" \
+    "${AUTHORIZATION_HEADER[@]}" \
     --data-raw "{ \"config\":  ${INITIAL_DATA}  }"    )
 
 if [ $? -ne 0 ]; then
